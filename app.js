3ebc79c6acd09cc55cecd5d94b85c5b0481aeb64
Adela015 <ade.cervantes01@gmail.com>
2022-04-06 18:52:29 -0300

    cors mejorado

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mainController = require('./src/controllers/mainController');
 const userLoggedMiddleware = require('./src/middlewares/userLoggedMiddleware');
 const session = require('express-session');
 const cookie = require('cookie-parser');
-
+const cors = require('cors')
 
 const port = 3003;
 
@@ -29,8 +29,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/src/views'));
 
 //MIDDLEWARES
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors());
 app.use(session({secret: 'maktamo', saveUninitialized: true, resave: false}));
 app.use(cookie());
 app.use(userLoggedMiddleware);
